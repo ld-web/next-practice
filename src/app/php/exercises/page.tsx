bb1d0274f7d3054f4e5e37d7b3300f26656dff9f
Lucas Delobelle <lucas@ld-web.net>
2024-03-27 13:47:05 +0100

    :children_crossing: increase practice zone width

--- src/app/php/exercises/page.tsx
+++ src/app/php/exercises/page.tsx
@@ -26,7 +26,7 @@ export default function Exercises() {
   }, []);
 
   return (
-    <section className="max-w-[900px] mx-auto mt-6">
+    <section className="max-w-[1000px] mx-auto mt-6">
       <header className="mb-6 md:mb-12 flex justify-between items-center">
         <h1 className="text-3xl md:text-5xl uppercase">PHP - Exercices</h1>
         <ThemeSwitcher />
