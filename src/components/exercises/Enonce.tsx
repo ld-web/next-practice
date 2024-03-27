3f593a22b37e8897b79b14d82538f0a74c910c35
Lucas Delobelle <lucas@ld-web.net>
2024-03-27 07:05:34 +0100

    :children_crossing: lighten states border color

--- src/components/exercises/Enonce.tsx
+++ src/components/exercises/Enonce.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Enonce = ({ children }: { children: ReactNode }) => (
-  <p className="p-2 border-l-4 border-blue-800 bg-slate-200 dark:bg-slate-700">
+  <p className="p-2 border-l-4 border-blue-500 bg-slate-200 dark:bg-slate-700">
     {children}
   </p>
 );
