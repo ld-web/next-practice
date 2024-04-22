af87e6d9ae9afe380e1e46bd916309b24aea87fb
Lucas Delobelle <lucas@ld-web.net>
2024-04-22 21:57:02 +0200

    :lipstick: update Statement light bg color

--- src/components/exercises/Enonce.tsx
+++ src/components/exercises/Enonce.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Enonce = ({ children }: { children: ReactNode }) => (
-  <div className="p-3 border-l-4 border-blue-500 bg-slate-200 dark:bg-slate-700">
+  <div className="p-3 border-l-4 border-blue-500 bg-sky-100 dark:bg-slate-700">
     {children}
   </div>
 );
