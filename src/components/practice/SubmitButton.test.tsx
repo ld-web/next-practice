1d2caf1d5bcd37dce9530740d270c32a54012e15
Lucas Delobelle <lucas@ld-web.net>
2024-04-28 17:54:15 +0200

    :wrench: Improve vitest setup
    TSConfig Globals

--- src/components/practice/SubmitButton.test.tsx
+++ src/components/practice/SubmitButton.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
 import SubmitButton from "./SubmitButton";
 
 describe("Practice Button", () => {
